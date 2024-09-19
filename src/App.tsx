1895c9b261e04e9b0cf6e15eaeeac8e97f9e8c2d
aespinos7 <aespinos7@cuc.edu.co>
2024-09-19 11:53:18 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import logo from './assets/img/imagen2.png'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={logo} alt="logo junior" />
+        <h1>Buenos Dias a todos</h1>
+        <p>un gusto a todos</p>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
